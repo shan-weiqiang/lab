d20d2e52a6f71fe507b01d67de9abf488908c0f8
shanweiqiang <schmessi@163.com>
2024-06-16 11:31:03 +0800

    work on websocket

--- websocketpp/client.cpp
+++ websocketpp/client.cpp
@@ -1,11 +1,17 @@
+#include "websocketpp/common/connection_hdl.hpp"
 #include "websocketpp/common/functional.hpp"
 #include "websocketpp/common/system_error.hpp"
 #include "websocketpp/config/asio_no_tls_client.hpp"
 #include "websocketpp/error.hpp"
 #include "websocketpp/logger/levels.hpp"
 #include "websocketpp/roles/client_endpoint.hpp"
+#include <atomic>
 #include <chrono>
+#include <condition_variable>
+#include <cstdio>
 #include <iostream>
+#include <mutex>
+#include <string>
 #include <thread>
 #include <websocketpp/client.hpp>
 
@@ -15,55 +21,73 @@ using websocketpp::lib::bind;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 
+std::mutex mtx;
+std::condition_variable cv;
+std::atomic_bool connected{false};
+
 typedef client::message_ptr message_ptr;
 void on_message(client *c, websocketpp::connection_hdl hdl, message_ptr msg) {
-  std::cout << "on_message called with hdl: " << hdl.lock().get()
-            << " and message: " << msg->get_payload() << std::endl;
-
-  websocketpp::lib::error_code ec;
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  c->send(hdl, msg->get_payload(), msg->get_opcode(), ec);
-  if (ec) {
-    std::cout << "Echo failed because: " << ec.message() << std::endl;
-  }
+  std::cout << "server: " << msg->get_payload()
+            << "; client thread id: " << std::this_thread::get_id()
+            << std::endl;
 }
 
 int main(int argc, char *argv[]) {
 
   client c;
   std::string uri = "ws://localhost:9002";
+
   if (argc == 2) {
     uri = argv[1];
   }
   try {
-    std::string input;
-    std::cout << "please input echo text: " << std::endl;
-    std::cin >> input;
+
     c.set_access_channels(websocketpp::log::alevel::all);
-    c.clear_access_channels(websocketpp::log::alevel::frame_payload);
+    c.clear_access_channels(websocketpp::log::alevel::all);
     c.init_asio();
     c.set_message_handler(bind(&on_message, &c, _1, _2));
+    c.set_open_handler([](websocketpp::connection_hdl hdl) {
+      // now main thread can send messages
+      std::lock_guard<std::mutex> lck{mtx};
+      connected.store(true);
+      cv.notify_one();
+    });
     websocketpp::lib::error_code ec;
     client::connection_ptr con = c.get_connection(uri, ec);
+    // client can connect to multi servers and the same time!!!
+    // client::connection_ptr con1 = c.get_connection("ws://localhost:9002",
+    // ec);
+
     if (ec) {
       std::cout << "could not create connection because: " << ec.message()
                 << std::endl;
       return 0;
     }
     c.connect(con);
+    // c.connect(con1);
 
     std::thread t([&c]() { c.run(); });
-    std::thread t1([](){
-        std::cout << "thread test" << std::endl;
-    });
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::thread t1([&c]() { c.run(); });
 
-    c.send(con->get_handle(), input, websocketpp::frame::opcode::text, ec);
-    if (ec) {
-      std::cout << "could not send message because: " << ec.message()
-                << std::endl;
-      return 0;
+    {
+      std::unique_lock<std::mutex> lck{mtx};
+      cv.wait(lck, [] { return connected.load(); });
     }
+    std::string input;
+    while (input != "exit") {
+      std::getline(std::cin, input);
+      std::cout << '\r';
+      std::cout << "client: " << input << std::endl;
+      fflush(stdout);
+      c.send(con->get_handle(), input, websocketpp::frame::opcode::text, ec);
+      if (ec) {
+        std::cout << "could not send message because: " << ec.message()
+                  << std::endl;
+        return 0;
+      }
+    }
+    c.stop();
+
     t.join();
     t1.join();
 
--- websocketpp/server.cpp
+++ websocketpp/server.cpp
@@ -1,4 +1,8 @@
 #include "websocketpp/common/connection_hdl.hpp"
+#include "websocketpp/frame.hpp"
+#include <string>
+#include <thread>
+#include <vector>
 #include <websocketpp/config/asio_no_tls.hpp>
 
 #include <websocketpp/server.hpp>
@@ -11,13 +15,16 @@ using websocketpp::lib::bind;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 
+std::vector<websocketpp::connection_hdl> clients;
+
 // pull out the type of messages sent by our config
 typedef server::message_ptr message_ptr;
 
 // Define a callback to handle incoming messages
 void on_message(server *s, websocketpp::connection_hdl hdl, message_ptr msg) {
-  std::cout << "on_message called with hdl: " << hdl.lock().get()
-            << " and message: " << msg->get_payload() << std::endl;
+  std::cout << "client: " << hdl.lock().get()
+            << "; msg content: " << msg->get_payload()
+            << "server thread id: " << std::this_thread::get_id() << std::endl;
 
   // check for a special command to instruct the server to stop listening so
   // it can be cleanly exited.
@@ -25,19 +32,13 @@ void on_message(server *s, websocketpp::connection_hdl hdl, message_ptr msg) {
     s->stop_listening();
     return;
   }
-
-  try {
-    s->send(hdl, msg->get_payload(), msg->get_opcode());
-  } catch (websocketpp::exception const &e) {
-    std::cout << "Echo failed because: "
-              << "(" << e.what() << ")" << std::endl;
-  }
 }
 
 void on_open(websocketpp::connection_hdl hdl) {
   std::cout << "<<<<<<<" << std::endl
             << "on_open called with hdl: " << hdl.lock().get() << std::endl
             << "<<<<<<<<<<" << std::endl;
+  clients.push_back(hdl);
 }
 
 int main() {
@@ -47,7 +48,7 @@ int main() {
   try {
     // Set logging settings
     echo_server.set_access_channels(websocketpp::log::alevel::all);
-    echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+    echo_server.clear_access_channels(websocketpp::log::alevel::all);
 
     // Initialize Asio
     echo_server.init_asio();
@@ -56,16 +57,28 @@ int main() {
     echo_server.set_message_handler(
         bind(&on_message, &echo_server, ::_1, ::_2));
     echo_server.set_open_handler(on_open);
-    
 
-    // Listen on port 9002
+    // Listen on port 9002; server can only listen one one port at the same
+    // time!!! but can get connected by multi clients
     echo_server.listen(9002);
 
     // Start the server accept loop
     echo_server.start_accept();
 
     // Start the ASIO io_service run loop
-    echo_server.run();
+    std::thread t = std::thread([&echo_server]() { echo_server.run(); });
+    std::thread t1 = std::thread([&echo_server]() { echo_server.run(); });
+
+    std::string input;
+    while (input != "exit") {
+      std::getline(std::cin, input);
+      for (websocketpp::connection_hdl &hdl : clients) {
+        echo_server.send(hdl, input, websocketpp::frame::opcode::text);
+      }
+    }
+
+    t.join();
+    t1.join();
   } catch (websocketpp::exception const &e) {
     std::cout << e.what() << std::endl;
   } catch (...) {
