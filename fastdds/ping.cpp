509f758053ed41f3ebd20276aa0369b37083f509
shanweiqiang <schmessi@163.com>
2024-06-23 10:39:03 +0800

    ping pong

--- fastdds/ping.cpp
+++ fastdds/ping.cpp
@@ -1,3 +1,4 @@
+#include "msg/HelloWorld.hpp"
 #include "msg/HelloWorldPubSubTypes.h"
 #include <chrono>
 #include <fastdds/dds/domain/DomainParticipant.hpp>
@@ -5,12 +6,30 @@
 #include <fastdds/dds/publisher/DataWriter.hpp>
 #include <fastdds/dds/publisher/Publisher.hpp>
 #include <fastdds/dds/publisher/qos/PublisherQos.hpp>
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+#include <fastdds/dds/subscriber/qos/SubscriberQos.hpp>
 #include <fastdds/dds/topic/Topic.hpp>
 #include <fastdds/dds/topic/TypeSupport.hpp>
 #include <thread>
 
 using namespace eprosima::fastdds::dds;
 using namespace HelloWorldModule;
+
+class echoHelloWorldListener : public DataReaderListener {
+public:
+  void on_data_available(DataReader *reader) override {
+    HelloWorld data;
+    SampleInfo info;
+
+    if (reader->take_next_sample(&data, &info) == RETCODE_OK) {
+      if (info.valid_data) {
+        std::cout << "Received message: " << data.message()<< std::endl;
+      }
+    }
+  }
+};
 
 int main() {
   // Create a DomainParticipant
@@ -27,13 +46,23 @@ int main() {
   Topic *topic = participant->create_topic("HelloWorldTopic", type->getName(),
                                            TOPIC_QOS_DEFAULT);
 
+  Topic *echo_topic = participant->create_topic("echoHelloWorldTopic", type->getName(),
+                                           TOPIC_QOS_DEFAULT);
+
   // Create a Publisher
   PublisherQos publisher_qos;
   Publisher *publisher = participant->create_publisher(publisher_qos);
 
+  SubscriberQos subscriber_qos;
+  Subscriber * subscriber = participant->create_subscriber(subscriber_qos);
+
   // Create a DataWriter
   DataWriterQos writer_qos;
   DataWriter *writer = publisher->create_datawriter(topic, writer_qos);
+
+  DataReaderQos reader_qos;
+  echoHelloWorldListener listener;
+  DataReader * reader = subscriber->create_datareader(echo_topic, reader_qos, &listener);
 
   // Create a sample data
   HelloWorld hello;
@@ -42,6 +71,7 @@ int main() {
   // Write the data
   for (;;) {
     writer->write(&hello);
+    std::cout << "Send message: " << hello.message() << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
--- fastdds/pong.cpp
+++ fastdds/pong.cpp
@@ -1,18 +1,26 @@
+
 #include "PayloadPubSubType.hpp"
 #include <fastdds/dds/domain/DomainParticipant.hpp>
 #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/publisher/qos/DataWriterQos.hpp>
+#include <fastdds/dds/publisher/qos/PublisherQos.hpp>
+#include <fastdds/dds/publisher/qos/WriterQos.hpp>
 #include <fastdds/dds/subscriber/DataReader.hpp>
 #include <fastdds/dds/subscriber/Subscriber.hpp>
 #include <fastdds/dds/subscriber/qos/SubscriberQos.hpp>
 #include <fastdds/dds/topic/Topic.hpp>
 #include <fastdds/dds/topic/TypeSupport.hpp>
 #include <fastdds/rtps/history/IPayloadPool.h>
+#include <memory>
 #include <thread>
 
 using namespace eprosima::fastdds::dds;
 
 class HelloWorldListener : public DataReaderListener {
 public:
+  HelloWorldListener(DataWriter *echo_writer) : writer_{echo_writer} {}
   void on_data_available(DataReader *reader) override {
     eprosima::fastdds::rtps::SerializedPayload_t data;
     SampleInfo info;
@@ -20,9 +28,12 @@ public:
     if (reader->take_next_sample(&data, &info) == RETCODE_OK) {
       if (info.valid_data) {
         std::cout << "Received message: " << data.length << std::endl;
+        // echo back payload data
+        writer_->write(&data);
       }
     }
   }
+  DataWriter *writer_;
 };
 
 int main() {
@@ -40,14 +51,20 @@ int main() {
   // Create a Topic
   Topic *topic = participant->create_topic("HelloWorldTopic", type->getName(),
                                            TOPIC_QOS_DEFAULT);
-
+  Topic *echo_topic = participant->create_topic(
+      "echoHelloWorldTopic", type->getName(), TOPIC_QOS_DEFAULT);
   // Create a Subscriber
   SubscriberQos subscriber_qos;
   Subscriber *subscriber = participant->create_subscriber(subscriber_qos);
 
+  PublisherQos pubscriber_qos;
+  Publisher *publisher = participant->create_publisher(pubscriber_qos);
+  DataWriterQos writer_qos;
+  DataWriter *writer = publisher->create_datawriter(echo_topic, writer_qos);
+
   // Create a DataReader
   DataReaderQos reader_qos;
-  HelloWorldListener listener;
+  HelloWorldListener listener{writer};
   DataReader *reader =
       subscriber->create_datareader(topic, reader_qos, &listener);
 
@@ -55,6 +72,7 @@ int main() {
   std::this_thread::sleep_for(std::chrono::seconds(10));
 
   // Cleanup
+  participant->delete_subscriber(subscriber);
   participant->delete_contained_entities();
   DomainParticipantFactory::get_instance()->delete_participant(participant);
 
